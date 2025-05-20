72c7962e6d1736b5e27df06c554d4bd74649a440
vipin-kumar735 <patelharimohan31@gmail.com>
2025-05-20 07:51:42 +0530

    Update play.py

--- EsproMusic/utils/inline/play.py
+++ EsproMusic/utils/inline/play.py
@@ -48,13 +48,13 @@ def stream_markup_timer(_, chat_id, played, dur):
         bar = "——————❥———"
     elif 70 <= umm < 80:
         bar = "———————❥——"
-    elif 80 <= umm < 95:
+    elif 80 <= umm < 95:      
         bar = "————————❥—"
     else:
         bar = "—————————❥"
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="▶︎", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
           #  InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
           #  InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
@@ -74,7 +74,7 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-          #  InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+          #  InlineKeyboardButton(text="▶︎", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
          #   InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
