8585ae94ff5f10a983efead4bbe395fafe6c482e
vipin-kumar735 <patelharimohan31@gmail.com>
2025-08-19 11:56:07 +0530

    Update Youtube.py

--- EsproMusic/platforms/Youtube.py
+++ EsproMusic/platforms/Youtube.py
@@ -8,8 +8,8 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
-from IstkharMusic.utils.database import is_on_off
-from IstkharMusic.utils.formatters import time_to_seconds
+from EsproMusic.utils.database import is_on_off
+from EsproMusic.utils.formatters import time_to_seconds
 import os
 import glob
 import random
